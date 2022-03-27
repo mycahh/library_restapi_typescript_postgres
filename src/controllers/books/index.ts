ad3994744b008613e84299a2f649d5de5ea14363
mycahh <mikaelfernandezj.d@gmail.com>
2022-03-27 16:16:50 -0400

    added all controllers

--- src/controllers/books/index.ts
+++ src/controllers/books/index.ts
@@ -1,22 +1,51 @@
 import { Request, Response } from "express"
+import { pool } from "../../database"
+import { QueryResult } from "pg"
+import { getListQuery, getBookByIdQuery, createBookQuery, updateBookQuery, deleteBookQuery } from "../../querys"
 
-export const getListBooks = (req: Request, res: Response) => {
-    res.send('Hola Peter')
+export const getListBooks = async (req: Request, res: Response) => {
+    try {
+        const response:QueryResult = await pool.query(getListQuery)
+
+        return res.status(200).json(response.rows)
+    } catch (error) {
+        res.status(500).json("Error")
+    }
 }
 
-export const getBook = (req: Request, res: Response) => {
+export const getBookById = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id)
+    const response:QueryResult = await pool.query(getBookByIdQuery, [id])
 
+    return res.json(response.rows)
 }
 
-export const deleteBook = (req: Request, res: Response) => {
+export const deleteBook = async (req: Request, res: Response) => {
+    const id = parseInt(req.params.id)
+    await pool.query(deleteBookQuery, [id])
     
+    res.json({
+        message: 'successfully eliminated'
+    })
+
 }
 
-export const updateBook = (req: Request, res: Response) => {
+export const updateBook = async(req: Request, res: Response) => {
+    const id = parseInt(req.params.id)
+    const { title, isbn, synopsis, genres, publicationyear } = req.body
+    await pool.query(updateBookQuery, [title, isbn, synopsis, genres, publicationyear, id])
 
+    res.json({
+        message: 'Successfully updated'
+    })
 }
 
-export const createBook = (req: Request, res: Response) => {
+export const createBook = async (req: Request, res: Response) => {
+    const { title, isbn, synopsis, genres, publicationyear } = req.body
+    const response:QueryResult = await pool.query(createBookQuery, [title, isbn, synopsis, genres, publicationyear])
 
+    res.json({
+        message: 'Successfully created'
+    })
 }
 
--- src/routes/books/index.ts
+++ src/routes/books/index.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express'
-import { getListBooks, getBook, createBook, updateBook, deleteBook } from '../../controllers/books'
+import { getListBooks, getBookById, createBook, updateBook, deleteBook } from '../../controllers/books'
 
 const router = Router()
 
 router.get('/', getListBooks)
-router.get('/:id', getBook)
+router.get('/:id', getBookById)
 router.post('/', createBook)
 router.put('/:id', updateBook)
 router.delete('/:id', deleteBook)
